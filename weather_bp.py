700254c481460fb8058c450eaddc69e0500b0fb9
Forrest McDougal <forrestmcdougal@gmail.com>
2019-02-21 20:03:27 -0500

    Delete weather_bp.py

--- weather_bp.py
+++
@@ -1,8 +0,0 @@
-from pprint import pprint
-import openweathermapy.core as owm
-from config import api_key
-
-def get_weather():
-    settings = {"units": "imperial", "appid": api_key}
-    zip_weather = owm.get_current(zip = zip, **settings)
-    pprint(zip_weather)
